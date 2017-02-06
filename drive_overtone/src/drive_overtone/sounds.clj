5206e04ff6d65da0f620cb050786d48a80316181
Jason Shiverick <JShiverick@tesla.com>
2017-02-06 10:33:18 -0800

    updated sounds in tidal and overtone

--- drive_overtone/src/drive_overtone/sounds.clj
+++ drive_overtone/src/drive_overtone/sounds.clj
@@ -1,8 +1,28 @@
 (ns drive-overtone.sounds)
 (use 'overtone.live)
 (use 'overtone.inst.sampled-piano)
+(use 'overtone.inst.sampled-flute)
+
+(sampled-piano)
 
 
+(on-event [:midi :note-on]
+          (fn [e]
+            (let [note (:note e)
+                  vel (:velocity-f e)]
+              (saw-wave (midi->hz note)  vel )))
+          :event-printer)
+
+(midi->hz (note :C3))
+(midi->hz (note :D2))
+(hz->midi 200)
+(stop)
+(dark-sea-horns :freq (midi->hz (note :E5)) :amp 0.1)
+(dark-sea-horns :freq (midi->hz (note :G3)) :amp 0.1)
+(dark-sea-horns :freq (midi->hz (note :B2)) :amp 0.1)
+(dark-sea-horns :freq (midi->hz (note :E2)) :amp 0.2)
+(hz->midi 200)
+(stop)
 (definst sin-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
      (sin-osc freq)
@@ -11,13 +31,13 @@
 
 
 (definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
-  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+  (* (env-gen (lin attack sustain release) 1 1 0.8 0.2 FREE)
      (saw freq)
      vol))
 (saw-wave)
 
 (definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
-  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+  (* (env-gen (lin attack sustain release) 1 1 1 0.2 FREE)
      (lf-pulse:ar freq)
      vol))
 (square-wave)
@@ -29,20 +49,28 @@
 (noisey)
 
 (definst triangle-wave [freq 440 attack 0.01 sustain 0.1 release 0.4 vol 0.4]
-  (* (env-gen (lin attack sustain release) 1 1 0 1 FREE)
+  (* (env-gen (lin attack sustain release) 1 1 1 0.4 FREE)
      (lf-tri freq)
      vol))
 (triangle-wave)
 
 
+(stop)
 
+(dark-sea-horns :freq 640 :amp 0.1)
+(dark-sea-horns :freq 200 :amp 0.1)
+(dark-sea-horns :freq 120 :amp 0.1)
+(dark-sea-horns :freq 80 :amp 0.2)
+
+(hz->midi 200)
+(midi->hz 55)
 (defsynth dark-sea-horns
   "Dark, rough and sharp sea horn.
    Without any attempt to use recursion (recursive example lacked the feedback feel)"
   [out-bus 0 amp 1 freq 65]
   (let [a (tanh (* 6 (lf-noise1:ar 3) (sin-osc:ar freq (* (lf-noise1:ar 0.1) 3))))
 
-        a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
+;14M[<64;29;14M4M<64;29;14M        a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
         a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
         a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
         a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
@@ -137,3 +165,45 @@
         a (allpass-l:ar a 0.3 [(+ (ranged-rand 0 0.2) 0.1) (+ (ranged-rand 0 0.2) 0.1)] 5)
         a (tanh a)]
     (out out-bus (* amp [ a a]))))
+
+
+(stop)
+(demo 60
+      (let [bpm     120
+            ;; create pool of notes as seed for random base line sequence
+            notes   [40 41 28 28 28 27 25 35 78]
+            ;; create an impulse trigger firing once per bar
+            trig    (impulse:kr (/ bpm 120))
+            ;; create frequency generator for a randomly picked note
+            freq    (midicps (lag (demand trig 0 (dxrand notes INF)) 0.25))
+            ;; switch note durations
+            swr     (demand trig 0 (dseq [1 6 6 2 1 2 4 8 3 3] INF))
+            ;; create a sweep curve for filter below
+            sweep   (lin-exp (lf-tri swr) -1 1 40 3000)
+            ;; create a slightly detuned stereo sawtooth oscillator
+            wob     (mix (saw (* freq [0.99 1.01])))
+            ;; apply low pass filter using sweep curve to control cutoff freq
+            wob     (lpf wob sweep)
+            ;; normalize to 80% volume
+            wob     (* 0.8 (normalizer wob))
+            ;; apply band pass filter with resonance at 5kHz
+            wob     (+ wob (bpf wob 1500 2))
+            ;; mix in 20% reverb
+            wob     (+ wob (* 0.2 (g-verb wob 9 0.7 0.7)))
+
+            ;; create impulse generator from given drum pattern
+            kickenv (decay (t2a (demand (impulse:kr (/ bpm 30)) 0 (dseq [1 0 0 0 0 0 1 0 1 0 0 1 0 0 0 0] INF))) 0.7)
+            ;; use modulated sine wave oscillator
+            kick    (* (* kickenv 7) (sin-osc (+ 40 (* kickenv kickenv kickenv 200))))
+            ;; clip at max volume to create distortion
+            kick    (clip2 kick 1)
+
+            ;; snare is just using gated & over-amplified pink noise
+            snare   (* 3 (pink-noise) (apply + (* (decay (impulse (/ bpm 240) 0.5) [0.4 2]) [1 0.05])))
+            ;; send through band pass filter with peak @ 2kHz
+            snare   (+ snare (bpf (* 4 snare) 2000))
+            ;; also clip at max vol to distort
+            snare   (clip2 snare 1)]
+   ;; mixdown & clip
+   (clip2 (+ wob kick snare) 1)))
+4MM
